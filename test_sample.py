ef1f1a9e78cbf11e7ba65113da4bc1ec225cfc22
evensty <maxim.veselov11@gmail.com>
2022-12-04 14:27:57 +0300

    add new string and google search test

--- test_sample.py
+++ test_sample.py
@@ -1,21 +1,29 @@
 from selene.support.shared import browser
-from selene import be
+from selene import be, have
 
 
+s = 'short text fot test'
 def test_should_be_google_search_input():
     browser.open('https://www.google.com/')
-    browser.element('[name ="q"]').should(be.blank)
+    browser.element('[name ="q"]').should(be.blank).type('habr').press_enter()
+    browser.element('[id="search"]').should(have.text('Все публикации подряд'))
 
 
-def test_example_0():
+def test_example():
     assert 10 > 1
+    assert 4 < 9
 
 
 def test_another_1():
     assert 0 != 1
+    assert 5 > 2
 
 
-def test_another_10():
+def test_another_11():
     assert 0 == 0
+
+
+def test_len_of_test_string():
+    assert len(s) > 10
 
 
